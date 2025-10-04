e15688136695885404deae82be1955d40b3d982c
Hermann Richy <hermannrichy15@gmail.com>
2025-10-04 12:58:30 +0100

    add splashscreen and next-theme for light/dark mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,21 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
+import SplashScreen from "@/components/section/SplashScreen";
 
 export default function Home() {
+    const [loading, setLoading] = useState(true);
+
     return (
-        <div className="flex justify-center items-center h-screen bg-sky-100 w-sm max-w-md p-4 shadow-lg">
-            <h1 className="font-rosnoc text-2xl">AEROGUARD</h1>
-        </div>
+        <>
+            {/* SplashScreen */}
+            {loading && <SplashScreen onFinish={() => setLoading(false)} />}
+
+            {/* Contenu principal */}
+            {!loading && (
+                <div className="flex justify-center items-center h-screen bg-sky-100 mx-auto w-sm max-w-md p-4 shadow-lg">
+                    <h1 className="font-rosnoc text-2xl">AEROGUARD</h1>
+                </div>
+            )}
+        </>
     );
 }
