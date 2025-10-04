0796597e6bd88f88b0a51e5a5e110e4226ce365a
adandeigor <adandehustler@gmail.com>
2025-10-04 12:04:49 +0100

    feat: ajout de la navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex justify-center items-center h-screen bg-sky-100 w-sm max-w-md p-4 shadow-lg">
+        <div className="flex justify-center items-center h-screen  shadow-lg">
             <h1 className="font-rosnoc text-2xl">AEROGUARD</h1>
         </div>
     );
